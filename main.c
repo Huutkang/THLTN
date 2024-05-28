57f6ea1e899c8c8fbac6f284087ed15938562baa
Nguyễn Hữu Thắng <nguyenhuuthang011@gmail.com>
2024-05-28 15:56:41 +0700

    first commint

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ * Created: 28/05/2024 3:52:42 CH
+ * Author: Thang
+ */
+
+#include <mega328p.h>
+
+void main(void)
+{
+while (1)
+    {
+    // Please write your application code here
+
+    }
+}
