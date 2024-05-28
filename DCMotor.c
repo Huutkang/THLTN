a7077eb55e469a2e6d5d2edb41de5fd252971d66
Thaidaongoc24 <92461962+Thaidaongoc24@users.noreply.github.com>
2024-05-28 13:09:15 +0700

    Add files via upload

---
+++ DCMotor.c
@@ -0,0 +1,42 @@
+#include "DCMotor.h"
+
+DCMotor::DCMotor(uint8_t pinA, uint8_t pinB, uint8_t pinS) {
+  this->pinA = pinA;
+  this->pinB = pinB;
+  this->pinS = pinS;
+
+  pinMode(this->pinA, OUTPUT);
+  pinMode(this->pinB, OUTPUT);
+  pinMode(this->pinS, OUTPUT);
+}
+
+
+// Turn the motor on with a specified speed.
+void DCMotor::on(uint8_t speed) {
+  this->off();
+
+  // Ensure speed is within valid range (0-255)
+  speed = constrain(speed, -255, 255);
+      
+  // Set the motor direction based on the sign of the speed
+  digitalWrite(this->pinA, speed >= 0 ? HIGH : LOW);
+  digitalWrite(this->pinB, speed >= 0 ? LOW : HIGH);
+
+  // Set the motor speed using PWM
+  analogWrite(this->pinS, abs(speed));
+}
+
+// Turn the motor on with a specified speed, for a given time.
+void DCMotor::on(uint8_t speed, int millisec) {
+  this->on(speed);
+  delay(millisec);
+  this->off();
+}
+
+// Turn the motor off.
+void DCMotor::off() {
+  digitalWrite(this->pinA, LOW);
+  digitalWrite(this->pinB, LOW);
+  analogWrite(this->pinS, 0);
+}
+
