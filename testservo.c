a50eb4ca9d5d123c519442dfaf6a7782543517b0
Thaidaongoc24 <92461962+Thaidaongoc24@users.noreply.github.com>
2024-06-02 01:16:43 +0700

    Update testservo.c

--- testservo.c
+++ testservo.c
@@ -1,13 +1,26 @@
-// test servo
 #include "servo.h"
+#include <delay.h>
 
-void main() {
-    servo_init(PD5); // Khoi tao voi chan PD5
+void main(void)
+{
+    // Khởi tạo servo trên chân PD5
+    servo_init(&PORTD, 5); 
 
-    while (1) {
-        servo_write(90); // Quay servo den goc 90 do
-        delay_ms(1000);
-        servo_write(0); // Quay servo den goc 0 do
-        delay_ms(1000);
+    while (1)
+    {
+        // Di chuyển servo qua lại giữa 0 và 180 độ
+        for (unsigned char angle = 0; angle <= 180; angle++)
+        {
+            servo_set_angle(angle);
+            servo_update();
+            delay_ms(15); // Điều chỉnh độ trễ để thay đổi tốc độ quay
+        }
+
+        for (unsigned char angle = 180; angle > 0; angle--)
+        {
+            servo_set_angle(angle);
+            servo_update();
+            delay_ms(15);
+        }
     }
 }
