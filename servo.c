1a415da16566e493fdbc192ce98a9dfc372ff2b1
Thaidaongoc24 <92461962+Thaidaongoc24@users.noreply.github.com>
2024-06-02 01:15:28 +0700

    Update servo.c

--- servo.c
+++ servo.c
@@ -1,26 +1,40 @@
 #include "servo.h"
 
-// Cac bien toan cuc de luu cau hinh
-uint8_t servo_pin;        // Chan dieu khien servo (VD: PD5)
+static volatile uint8_t *servo_port; // Con tro toi port dieu khien servo
+static uint8_t servo_pin; // Pin dieu khien servo
+static volatile unsigned char target_angle;
+static volatile unsigned long previous_millis;
 
-void servo_init(uint8_t pin) {
+void servo_init(volatile uint8_t *port, uint8_t pin)
+{
+    servo_port = port; // Luu lai port va pin
     servo_pin = pin;
 
-    // Cau hinh Timer1
-    DDRD |= (1 << servo_pin);      // Dat chan servo la OUTPUT
-    TCCR1A |= (1 << COM1A1);      // Che do PWM khong dao pha (non-inverting)
-    TCCR1B |= (1 << CS11);       // Prescaler 8
+    // Thiet lap chan dieu khien servo la OUTPUT
+    *servo_port |= (1 << servo_pin);
 
-    // Gia tri mac dinh cho tan so va do rong xung
-    ICR1 = 20000; // Tan so PWM 50Hz (20ms)
+    // Thiet lap Timer2 de tao tin hieu PWM
+    TCCR2A = (1 << COM2A1) | (1 << WGM21) | (1 << WGM20); // Fast PWM
+    TCCR2B = (1 << CS21); // Prescaler 8
+    OCR2A = 125; // Khoi tao gia tri ban dau (tuong ung goc 90 do)
+
+    // Khoi tao bien thoi gian
+    previous_millis = millis();
 }
 
-void servo_write(uint8_t angle) {
-    // Tinh toan do rong xung PWM dua tren goc quay
-    uint16_t pulse_width = 500 + (angle * 10); // 500us (0 do) - 2500us (180 do)
-
-    // Dat thanh ghi OCR1A
-    OCR1A = pulse_width;
+void servo_set_angle(unsigned char angle)
+{
+    target_angle = angle;
 }
 
-// (Ham servo_read() khong thay doi)
+void servo_update(void)
+{
+    unsigned long current_millis = millis();
+
+    if (current_millis - previous_millis >= 20)
+    {
+        unsigned int pulse_width = 1000 + (target_angle * 10);
+        OCR2A = pulse_width / 8; // Chia cho 8 do prescaler la 8
+        previous_millis = current_millis;
+    }
+}
